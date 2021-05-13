bfe93357e69f4b15db85a770d305ef961e2e4842
Filippo Garota <filogarota101@gmail.com>
2021-05-13 16:55:03 +0200

    Stampo i dati della tabella nel file blade

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
     public function index() {
-        return view('welcome');
+        $movies = Movie::all();
+        return view('movie', ['movies' => $movies]);
     }
 }
