aaabe0f04496fb560d02af87daa81e35169c2500
Filippo Garota <filogarota101@gmail.com>
2021-05-14 22:06:03 +0200

    file scss e stile dell'header

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,16 +4,36 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
   <title>Document</title>
 </head>
 <body>
-  <h1>Tutti i film</h1>
+  <header>
+    <div class="logo">
+      <img src="{{asset('images/hotpopcornmovie.svg')}}" alt="">
+    </div>
+    <div class="brand-wrapper">
+      <h2>The Movie Pub</h2>
+    </div>
+    <nav class="main-nav">
+      <ul>
+        <li><a href="#">link</a></li>
+        <li><a href="#">link</a></li>
+        <li><a href="#">link</a></li>
+      </ul>
+    </nav>
+  </header>
+    <div class="title">
+      <h1>Tutti i film</h1>
+    </div>
     @foreach ($movies as $movie)
-      <h3>{{$movie->title}}</h3>
-      <h4>{{$movie->author}}</h4>
-      <p>{{$movie->genre}}</p>
-      <img src="{{$movie->poster}}" alt="">
-      <a href="{{route('movies.show', ['movie' => $movie->id])}}">Dettaglio</a>
+      <div class="card">
+        <h3>{{$movie->title}}</h3>
+        <h4>{{$movie->author}}</h4>
+        <p>{{$movie->genre}}</p>
+        <img src="{{$movie->poster}}" alt="">
+        <a href="{{route('movies.show', ['movie' => $movie->id])}}">Dettaglio</a>
+      </div>
     @endforeach
 </body>
 </html>
